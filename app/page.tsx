458e25f6fdb0b0587060170701680ce681d852ee
Krish Sharma <krishsharma3355@gmail.com>
2026-04-16 22:47:15 +0530

    Left-Text-Part

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import About from "./modules/components/About";
 import Skill from "./modules/components/Skill";
 import Project from "./modules/components/Project";
 import { useState, useEffect } from "react";
+import ContactUs from "./modules/components/ContactUs";
 
 const page = () => {
   const [loading, setLoading] = useState(true);
@@ -39,6 +40,9 @@ const page = () => {
         </section>
         <section id="skill">
           <Skill />
+        </section>
+        <section id="contactUs">
+          <ContactUs />
         </section>
       </div>
     </>
